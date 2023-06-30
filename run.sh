69a912a9b8b7642b7c7353763a27ca4dc78d5703
Zhong Dongyu <zhong.dongyu0521@gmail.com>
2023-06-30 09:52:18 +0800

    zdy commit

--- run.sh
+++ run.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-cp -rf ./data/dataset_info.json /root/workspace_law/data_ChatGLM
+# cp -rf ./data/dataset_info.json /root/workspace_law/data_ChatGLM
+# rm -rf /root/workspace_law/data_ChatGLM/output/.*
 
-export CUDA_VISIBLE_DEVICES=0
-python3 ./src/train_sft.py \
-    --model_name_or_path /root/workspace_law/THUDM-chatglm2-6b \
-    --use_v2 \
-    --do_train \
-    --dataset 52k,92k \
-    --dataset_dir /root/workspace_law/data_ChatGLM \
-    --finetuning_type lora \
-    --output_dir /root/workspace_law/data_ChatGLM/output \
-    --overwrite_cache \
-    --per_device_train_batch_size 12 \
-    --gradient_accumulation_steps 2 \
-    --lr_scheduler_type cosine \
-    --logging_steps 10 \
-    --save_steps 1000 \
-    --warmup_steps 0 \
-    --learning_rate 1e-3 \
-    --num_train_epochs 4 \
-    --fp16 
-
-
-# python3 src/cli_demo.py \
+# export CUDA_VISIBLE_DEVICES=0
+# python3 ./src/train_sft.py \
 #     --model_name_or_path /root/workspace_law/THUDM-chatglm2-6b \
 #     --use_v2 \
-#     --checkpoint_dir /root/workspace_law/data_ChatGLM/output_export \
+#     --do_train \
+#     --dataset 52k,92k \
+#     --dataset_dir /root/workspace_law/data_ChatGLM \
+#     --finetuning_type lora \
+#     --output_dir /root/workspace_law/data_ChatGLM/output \
+#     --overwrite_cache \
+#     --per_device_train_batch_size 12 \
+#     --gradient_accumulation_steps 2 \
+#     --lr_scheduler_type cosine \
+#     --logging_steps 10 \
+#     --save_steps 1000 \
+#     --warmup_steps 0 \
+#     --learning_rate 1e-3 \
+#     --num_train_epochs 4 \
+#     --fp16 \
+#     --lora_rank 32 \
+
+
+python3 src/cli_demo.py \
+    --model_name_or_path /root/workspace_law/THUDM-chatglm2-6b \
+    --use_v2 \
+    --checkpoint_dir /root/workspace_law/data_ChatGLM/output_export \
     
     
 
